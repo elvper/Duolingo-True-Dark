81fdbd00ff21f28f32d85c6fcb28557894f0c02e
elvper <jefdelaeu@gmail.com>
2017-11-12 03:11:28 +0100

    Create DuoTrueDark.js

---
+++ DuoTrueDark.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Duolingo True Dark
+// @description Turn off the lights on Duolingo
+// @include     https://www.duolingo.com/*
+// @version     0
+// @author      elvper
+// ==/UserScript==
+
+var brightness = 0.5; // between 0 and 1, 1 is no adjustment while 0 is completely black.
+
+var bodydiv = document.createElement('div');
+	bodydiv.id = 'TurnDownLights';
+	document.getElementsByTagName('body')[0].appendChild(bodydiv);
+
+document.head.innerHTML += '<style type="text/css">'+
+
+// ##### overlay to darken #####
+'#TurnDownLights{position:fixed;width:100%;height:100%;background-color:#000;top:0;left:0;z-index:9001;opacity:' + (1 - brightness) + ';pointer-events:none}' +
+
+'</style>';
